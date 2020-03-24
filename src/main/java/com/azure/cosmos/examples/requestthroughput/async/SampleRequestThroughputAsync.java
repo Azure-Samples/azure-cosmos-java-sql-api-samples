9226c2132b74d0ee591ff56a541ded42d1174829
Andrew Feldman <andrew.feldman@microsoft.com>
2020-03-23 21:45:13 -0700

    Manually merged changes to Worked App Sample and request throughput sample

--- src/main/java/com/azure/cosmos/examples/requestthroughput/async/SampleRequestThroughputAsync.java
+++ src/main/java/com/azure/cosmos/examples/requestthroughput/async/SampleRequestThroughputAsync.java
@@ -10,8 +10,11 @@ import com.azure.cosmos.examples.common.AccountSettings;
 import com.azure.cosmos.examples.common.Profile;
 import com.azure.cosmos.models.CosmosContainerProperties;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.util.concurrent.AtomicDouble;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
@@ -36,6 +39,7 @@ public class SampleRequestThroughputAsync {
     private static AtomicBoolean resources_created = new AtomicBoolean(false);
     private static AtomicInteger number_docs_inserted = new AtomicInteger(0);
     private static AtomicBoolean resources_deleted = new AtomicBoolean(false);
+    private static AtomicDouble total_charge = new AtomicDouble(0.0);
 
     public static void requestThroughputDemo() {
         ConnectionPolicy my_connection_policy = ConnectionPolicy.getDefaultPolicy();
@@ -52,12 +56,12 @@ public class SampleRequestThroughputAsync {
         // Describe the logic of database and container creation using Reactor...
         Mono<Void> databaseContainerIfNotExist = client.createDatabaseIfNotExists("ContosoInventoryDB").flatMap(databaseResponse -> {
             database = databaseResponse.getDatabase();
-            logger.info("Got DB.");
+            logger.info("\n\n\n\nCreated database ContosoInventoryDB.\n\n\n\n");
             CosmosContainerProperties containerProperties = new CosmosContainerProperties("ContosoInventoryContainer", "/id");
-            return database.createContainerIfNotExists(containerProperties, 400);
+            return database.createContainerIfNotExists(containerProperties, 100000);
         }).flatMap(containerResponse -> {
             container = containerResponse.getContainer();
-            logger.info("Got container.");
+            logger.info("\n\n\n\nCreated container ContosoInventoryContainer.\n\n\n\n");
             return Mono.empty();
         });
 
@@ -81,29 +85,49 @@ public class SampleRequestThroughputAsync {
 
         // Insert many docs into container...
         logger.info("Inserting {} documents...", number_of_docs);
-        docs.forEach(doc -> {
-            try {
-                Thread.sleep(12);
-            } catch (Exception err) {
-                logger.error("Error throttling programmatically: ",err);
-            }
-            // ...by describing logic of item insertion using Reactor. Then subscribe() to execute.
-            container.createItem(doc)
-                    // ^Publisher: upon subscription, createItem inserts a doc &
-                    // publishes request response to the next operation...
-                    .flatMap(itemResponse -> {
-                        // ...Streaming operation: count each doc & check success...
-                        if (itemResponse.getStatusCode() == 201)
-                            number_docs_inserted.getAndIncrement();
-                        else
-                            logger.warn("WARNING insert status code {} != 201", itemResponse.getStatusCode());
-                        return Mono.empty();
-                    }).subscribe(); // ...Subscribing to the publisher triggers stream execution.
-        });
+
+        Profile.tic();
+        int last_docs_inserted=0;
+        double last_total_charge=0.0;
+
+        Flux.fromIterable(docs).flatMap(doc -> container.createItem(doc))
+                // ^Publisher: upon subscription, createItem inserts a doc &
+                // publishes request response to the next operation...
+                .flatMap(itemResponse -> {
+                    // ...Streaming operation: count each doc & check success...
+
+                    if (itemResponse.getStatusCode() == 201) {
+                        number_docs_inserted.getAndIncrement();
+                        total_charge.getAndAdd(itemResponse.getRequestCharge());
+                    }
+                    else
+                        logger.warn("WARNING insert status code {} != 201", itemResponse.getStatusCode());
+                    return Mono.empty();
+                }).subscribe(); // ...Subscribing to the publisher triggers stream execution.
 
         // Do other things until async response arrives
         logger.info("Doing other things until async doc inserts complete...");
-        while (number_docs_inserted.get() < number_of_docs) Profile.doOtherThings();
+        //while (number_docs_inserted.get() < number_of_docs) Profile.doOtherThings();
+        double toc_time=0.0;
+        int current_docs_inserted=0;
+        double current_total_charge=0.0, rps=0.0, rups=0.0;
+        while (number_docs_inserted.get() < number_of_docs) {
+            toc_time=Profile.toc_ms();
+            current_docs_inserted=number_docs_inserted.get();
+            current_total_charge=total_charge.get();
+            if (toc_time >= 1000.0) {
+                Profile.tic();
+                rps=1000.0*((double)(current_docs_inserted-last_docs_inserted))/toc_time;
+                rups=1000.0*(current_total_charge-last_total_charge)/toc_time;
+                logger.info(String.format("\n\n\n\n" +
+                        "Async Throughput Profiler Result, Last 1000ms:" + "\n\n" +
+                        "%8s          %8s", StringUtils.center("Req/sec",8),StringUtils.center("RU/s",8)) + "\n"
+                        + "----------------------------------" + "\n"
+                        + String.format("%8.1f          %8.1f",rps,rups) + "\n\n\n\n");
+                last_docs_inserted=current_docs_inserted;
+                last_total_charge=current_total_charge;
+            }
+        }
 
         // Inserts are complete. Cleanup (asynchronously!)
         logger.info("Deleting resources.");
@@ -123,5 +147,6 @@ public class SampleRequestThroughputAsync {
         logger.info("Closing client...");
         client.close();
         logger.info("Done with demo.");
+
     }
 }
--- src/main/java/com/azure/cosmos/examples/requestthroughput/sync/SampleRequestThroughput.java
+++ src/main/java/com/azure/cosmos/examples/requestthroughput/sync/SampleRequestThroughput.java
@@ -7,15 +7,24 @@ import com.azure.cosmos.CosmosClient;
 import com.azure.cosmos.CosmosClientBuilder;
 import com.azure.cosmos.CosmosContainer;
 import com.azure.cosmos.CosmosDatabase;
+import com.azure.cosmos.ThrottlingRetryOptions;
 import com.azure.cosmos.examples.common.AccountSettings;
 import com.azure.cosmos.examples.common.Profile;
 import com.azure.cosmos.models.CosmosContainerProperties;
 import com.azure.cosmos.models.CosmosItemResponse;
+import com.azure.cosmos.models.IndexingMode;
+import com.azure.cosmos.models.IndexingPolicy;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.util.concurrent.AtomicDouble;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class SampleRequestThroughput {
 
@@ -32,14 +41,24 @@ public class SampleRequestThroughput {
     private static CosmosClient client;
     private static CosmosDatabase database;
     private static CosmosContainer container;
+    private static AtomicInteger number_docs_inserted = new AtomicInteger(0);
+    private static AtomicDouble total_charge = new AtomicDouble(0.0);
+    private static int last_docs_inserted=0;
+    private static double last_total_charge=0.0;
+    private static double toc_time=0.0;
+    private static int current_docs_inserted=0;
+    private static double current_total_charge=0.0, rps=0.0, rups=0.0;
 
     public static void requestThroughputDemo() {
         ConnectionPolicy my_connection_policy = ConnectionPolicy.getDefaultPolicy();
+        ThrottlingRetryOptions retry_options = new ThrottlingRetryOptions();
+        //retry_options.setMaxRetryWaitTime(Duration.ZERO);
+        my_connection_policy.setThrottlingRetryOptions(retry_options);
 
         client = new CosmosClientBuilder()
                 .setEndpoint(AccountSettings.HOST)
                 .setKey(AccountSettings.MASTER_KEY)
-                .setConnectionPolicy(ConnectionPolicy.getDefaultPolicy())
+                .setConnectionPolicy(my_connection_policy)
                 .setConsistencyLevel(ConsistencyLevel.EVENTUAL)
                 .buildClient();
 
@@ -47,10 +66,14 @@ public class SampleRequestThroughput {
         // While the client waits for a response, this thread is blocked from
         // performing other tasks.
         database = client.createDatabaseIfNotExists("ContosoInventoryDB").getDatabase();
-        logger.info("Got DB.");
+        logger.info("\n\n\n\nCreated database ContosoInventoryDB.\n\n\n\n");
+        //IndexingPolicy indexingPolicy = new IndexingPolicy();
+        //indexingPolicy.setIndexingMode(IndexingMode.NONE);
+        //indexingPolicy.setAutomatic(false);
         CosmosContainerProperties containerProperties = new CosmosContainerProperties("ContosoInventoryContainer", "/id");
-        container = database.createContainerIfNotExists(containerProperties, 400).getContainer();
-        logger.info("Got container.");
+        //containerProperties.setIndexingPolicy(indexingPolicy);
+        container = database.createContainerIfNotExists(containerProperties, 100000).getContainer();
+        logger.info("\n\n\n\nCreated container ContosoInventoryContainer.\n\n\n\n");
         // Resources are ready.
         //
         // Create many docs to insert into the container
@@ -59,15 +82,42 @@ public class SampleRequestThroughput {
         ArrayList<JsonNode> docs = Profile.generateDocs(number_of_docs);
         logger.info("Inserting {} documents...", number_of_docs);
 
+        Profile.tic();
+
+        //Profiler code - it's good for this part to be async
+        Flux.interval(Duration.ofMillis(10)).map(tick -> {
+            //logger.info("In profiler.");
+            toc_time=Profile.toc_ms();
+            current_docs_inserted=number_docs_inserted.get();
+            current_total_charge=total_charge.get();
+            if (toc_time >= 1000.0) {
+                Profile.tic();
+                rps=1000.0*((double)(current_docs_inserted-last_docs_inserted))/toc_time;
+                rups=1000.0*(current_total_charge-last_total_charge)/toc_time;
+                logger.info(String.format("\n\n\n\n" +
+                        "Sync Throughput Profiler Result, Last 1000ms:" + "\n\n" +
+                        "%8s          %8s", StringUtils.center("Req/sec",8),StringUtils.center("RU/s",8)) + "\n"
+                        + "----------------------------------" + "\n"
+                        + String.format("%8.1f          %8.1f",rps,rups) + "\n\n\n\n");
+                last_docs_inserted=current_docs_inserted;
+                last_total_charge=current_total_charge;
+            }
+            return tick;
+        }).subscribe();
+
         // Insert many docs synchronously.
         // The client blocks waiting for a response to each insert request,
         // which limits throughput.
         // While the client is waiting for a response, the thread is blocked from other tasks
-        docs.forEach(doc -> {
+        for(JsonNode doc : docs) {
             CosmosItemResponse<JsonNode> itemResponse = container.createItem(doc);
-            if (itemResponse.getStatusCode() != 201)
+            if (itemResponse.getStatusCode() == 201) {
+                number_docs_inserted.getAndIncrement();
+                total_charge.getAndAdd(itemResponse.getRequestCharge());
+            }
+            else
                 logger.warn("WARNING insert status code {} != 201", itemResponse.getStatusCode());
-        });
+        }
 
         // Clean up
         logger.info("Deleting resources.");
@@ -79,5 +129,6 @@ public class SampleRequestThroughput {
         client.close();
 
         logger.info("Done with demo.");
+
     }
 }
