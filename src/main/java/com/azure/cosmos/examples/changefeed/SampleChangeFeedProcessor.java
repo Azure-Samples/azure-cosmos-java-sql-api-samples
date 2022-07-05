ae1fd2580343187c104c01887f8ab7edd1b65534
Theo van Kraay <thvankra@microsoft.com>
2022-07-05 17:27:09 +0100

    remove unused import

--- src/main/java/com/azure/cosmos/examples/changefeed/SampleChangeFeedProcessor.java
+++ src/main/java/com/azure/cosmos/examples/changefeed/SampleChangeFeedProcessor.java
@@ -22,7 +22,6 @@ import com.azure.cosmos.models.ThroughputProperties;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.module.afterburner.asm.Opcodes;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
